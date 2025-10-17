57784b43b0fb97255d4558da18e50c506a61cc51
DlnKot <prod408@yandex.ru>
2025-10-17 17:06:51 +0300

    Fix bugs

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: '/your-repo-name/', // ЗАМЕНИ на имя своего репозитория!
+    base: '/xtravault/',
     build: {
         outDir: 'dist'
     }
